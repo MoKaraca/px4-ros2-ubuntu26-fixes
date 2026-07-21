64d6054d752b8a164afe2d15accfa0a046954919
MoKaraca <MoKaraca@users.noreply.github.com>
2026-07-21 14:06:55 +0300

    fix: added colcon alias to solve python CMake issues

--- setup_ros2_env.sh
+++ setup_ros2_env.sh
@@ -10,4 +10,7 @@ if [ -f "$MAMBA_ROOT_PREFIX/envs/ros2_humble/setup.bash" ]; then
     source "$MAMBA_ROOT_PREFIX/envs/ros2_humble/setup.bash"
 fi
 
+# Alias colcon to automatically use the Conda python executable to avoid CMake errors
+alias colcon="colcon build --cmake-args -DPython3_EXECUTABLE=\$(which python3) -DPython3_INCLUDE_DIR=\$CONDA_PREFIX/include/python3.12 -DPython_EXECUTABLE=\$(which python3) -DPython_INCLUDE_DIR=\$CONDA_PREFIX/include/python3.12"
+
 echo "ROS 2 Humble (via Micromamba) activated successfully."
